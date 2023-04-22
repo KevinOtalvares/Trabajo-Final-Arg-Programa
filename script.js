b7c5de2f5de7e84f4df9656cf4b8d316296c220d
Kevin-Otalvares <kevinotalvares@gmail.com>
2023-04-22 18:26:55 -0300

    Estilizar sobre-nosotros.html y hacer calculadora de helados funcional

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function iniciarMap(){
         map: map
     });
 }
+
+function calcularHelado(){
+    let listaHelado = document.getElementById("listaHelado");
+    let cantInvitados = Number(document.getElementById("cantInvitados").value);
+    
+    let cantCucuruchos = cantInvitados + Math.floor(cantInvitados/10);
+    let cantPaletas = cantInvitados + Math.floor(cantInvitados/10);
+    let cantKilos = Math.floor(cantInvitados / 4);
+    if (cantInvitados % 4 != 0) {
+        cantKilos++;
+    }
+    listaHelado.innerHTML = "<h6>Para una cantidad de " + cantInvitados + " invitados te recomendamos las siguientes opciones:</h6>";
+    listaHelado.innerHTML += "<li>" + cantKilos + " Kg de helado.</li>";
+    listaHelado.innerHTML += "<li>" + cantKilos + " Kg de helado + " + cantCucuruchos + " cucuruchos.</li>";
+    listaHelado.innerHTML += "<li>" + cantPaletas + " Lado Paletas®.</li>";
+    listaHelado.innerHTML += "<li>" + Math.ceil(cantKilos/2) + " Kg de Helado + " + Math.floor(cantPaletas/2) + " Lado Paletas®.</li>";
+}
